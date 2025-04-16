2cf60f5ce2338d5959601d6c66e9cbead543fc4e
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-15 19:36:33 -0500

    feat: create Series and inherit titulo

---
+++ src/com/jpproject/screenmatch/modelos/Series.java
@@ -0,0 +1,32 @@
+package com.jpproject.screenmatch.modelos;
+
+public class Series extends Titulo{
+
+    private int temporadas;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+}
