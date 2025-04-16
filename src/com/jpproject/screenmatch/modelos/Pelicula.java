1103383f59bf65f66d44d57f02f585f8b0a7ccdd
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-15 19:35:13 -0500

    feat: make pelicula inherit titulo

--- src/com/jpproject/screenmatch/modelos/Pelicula.java
+++ src/com/jpproject/screenmatch/modelos/Pelicula.java
@@ -1,68 +1,15 @@
 package com.jpproject.screenmatch.modelos;
 
-public class Pelicula {
+public class Pelicula extends Titulo{
 
-    private String titulo;
-    private int fechaDeLanzamiento;
-    private int duracionEnMinutos;
-    private boolean incluidoEnPlan;
-    private double sumaEvaluaciones;
-    private int cantidadEvaluaciones;
+    private String director;
 
-    public void mostrarFichaTecnica() {
-        System.out.println("Mi pelicula es: " + titulo);
-        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
-        System.out.println("Su duracion en minutos es: " + duracionEnMinutos);
+    public String getDirector() {
+        return director;
     }
 
-    public void evaluarPelicula(double nota){
-        sumaEvaluaciones += nota;
-        cantidadEvaluaciones++;
+    public void setDirector(String director) {
+        this.director = director;
     }
-
-    public double calcularMedia(){
-        return sumaEvaluaciones/cantidadEvaluaciones;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public int getFechaDeLanzamiento() {
-        return fechaDeLanzamiento;
-    }
-
-    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
-        this.fechaDeLanzamiento = fechaDeLanzamiento;
-    }
-
-    public int getDuracionEnMinutos() {
-        return duracionEnMinutos;
-    }
-
-    public void setDuracionEnMinutos(int duracionEnMinutos) {
-        this.duracionEnMinutos = duracionEnMinutos;
-    }
-
-    public boolean isIncluidoEnPlan() {
-        return incluidoEnPlan;
-    }
-
-    public void setIncluidoEnPlan(boolean incluidoEnPlan) {
-        this.incluidoEnPlan = incluidoEnPlan;
-    }
-
-    public double getSumaEvaluaciones() {
-        return sumaEvaluaciones;
-    }
-
-
-    public int getCantidadEvaluaciones() {
-        return cantidadEvaluaciones;
-    }
-
 }
+
