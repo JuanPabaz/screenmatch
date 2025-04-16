546bd59fb1fddcd90baf7f1c24ea88d0319e044b
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-16 09:04:21 -0500

    feat: add incluye method to CalculadoraDeTiempo

--- src/com/jpproject/screenmatch/calculos/CalculadoraDeTiempo.java
+++ src/com/jpproject/screenmatch/calculos/CalculadoraDeTiempo.java
@@ -1,4 +1,16 @@
 package com.jpproject.screenmatch.calculos;
 
+import com.jpproject.screenmatch.modelos.Titulo;
+
 public class CalculadoraDeTiempo {
+
+    private int tiempoTotal;
+
+    public void incluye(Titulo titulo){
+        tiempoTotal += titulo.getDuracionEnMinutos();
+    }
+
+    public int getTiempoTotal() {
+        return tiempoTotal;
+    }
 }
