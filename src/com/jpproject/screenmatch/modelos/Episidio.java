25a0493da71143bf3d96339436b3f72bfc7636b4
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-16 09:16:25 -0500

    feat: create Episodio

---
+++ src/com/jpproject/screenmatch/modelos/Episidio.java
@@ -0,0 +1,31 @@
+package com.jpproject.screenmatch.modelos;
+
+public class Episidio {
+    private int numero;
+    private String titulo;
+    private Series serie;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Series getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Series serie) {
+        this.serie = serie;
+    }
+}
