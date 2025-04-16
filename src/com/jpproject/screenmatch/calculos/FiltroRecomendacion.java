65b0fcddb5402d90322f64ce27e3b0ead3a0f240
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-16 09:27:09 -0500

    feat: create FiltroRecomendacion with filtrar method

---
+++ src/com/jpproject/screenmatch/calculos/FiltroRecomendacion.java
@@ -0,0 +1,14 @@
+package com.jpproject.screenmatch.calculos;
+
+public class FiltroRecomendacion {
+
+    void filtrar(Clasificable clasificable){
+        if(clasificable.getClasificicacion() >= 4){
+            System.out.println("Recomendado");
+        }else if(clasificable.getClasificicacion() >= 2){
+            System.out.println("Popular en el momento");
+        }else{
+            System.out.println("No Recomendado");
+        }
+    }
+}
