6c6e9280bba92f5eb588b2bf18757b3a75a6f480
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-15 19:34:59 -0500

    feat: create parent class titulo

---
+++ src/com/jpproject/screenmatch/modelos/Titulo.java
@@ -0,0 +1,67 @@
+package com.jpproject.screenmatch.modelos;
+
+public class Titulo {
+
+    private String titulo;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnPlan;
+    private double sumaEvaluaciones;
+    private int cantidadEvaluaciones;
+
+    public void mostrarFichaTecnica() {
+        System.out.println("Mi pelicula es: " + titulo);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Su duracion en minutos es: " + duracionEnMinutos);
+    }
+
+    public void evaluarPelicula(double nota){
+        sumaEvaluaciones += nota;
+        cantidadEvaluaciones++;
+    }
+
+    public double calcularMedia(){
+        return sumaEvaluaciones/cantidadEvaluaciones;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnPlan() {
+        return incluidoEnPlan;
+    }
+
+    public void setIncluidoEnPlan(boolean incluidoEnPlan) {
+        this.incluidoEnPlan = incluidoEnPlan;
+    }
+
+    public double getSumaEvaluaciones() {
+        return sumaEvaluaciones;
+    }
+
+
+    public int getCantidadEvaluaciones() {
+        return cantidadEvaluaciones;
+    }
+}
