3f7d37f258b27c80608c684cfaaaca1ec3a3ba02
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-17 21:17:26 -0500

    feat: create PrincipalConListas

---
+++ src/com/jpproject/screenmatch/principal/PrincipalConListas.java
@@ -0,0 +1,30 @@
+package com.jpproject.screenmatch.principal;
+
+import com.jpproject.screenmatch.modelos.Pelicula;
+import com.jpproject.screenmatch.modelos.Series;
+import com.jpproject.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalConListas {
+    public static void main(String[] args) {
+        Pelicula bastardosSinGloria = new Pelicula("Bastardos sin gloria", 2015);
+        bastardosSinGloria.evaluarPelicula(10);
+        Pelicula avatar = new Pelicula("Avatar", 2012);
+        avatar.evaluarPelicula(8);
+        var senorDeLosAnillos = new Pelicula("El señor de los anillos", 2001);
+        senorDeLosAnillos.evaluarPelicula(9);
+        Series casaDragon = new Series("La Casa Del Dragon", 2022);
+        casaDragon.evaluarPelicula(9);
+
+        ArrayList<Titulo> listaDeTitulos = new ArrayList<>();
+        listaDeTitulos.add(bastardosSinGloria);
+        listaDeTitulos.add(avatar);
+        listaDeTitulos.add(senorDeLosAnillos);
+        listaDeTitulos.add(casaDragon);
+
+        for (Titulo titulo : listaDeTitulos) {
+            System.out.println(titulo);
+        }
+    }
+}
