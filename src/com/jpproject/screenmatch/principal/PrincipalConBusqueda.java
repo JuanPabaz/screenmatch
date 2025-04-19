d138d5aac94bccfa6e2913e34b9afb453a16d935
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-19 16:02:16 -0500

    feat: create PrincipalConBusqueda and implement HTTPClient

---
+++ src/com/jpproject/screenmatch/principal/PrincipalConBusqueda.java
@@ -0,0 +1,28 @@
+package com.jpproject.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la pelicula a buscar: ");
+        var pelicula = sc.nextLine();
+        String url = "https://www.omdbapi.com/?t="+pelicula +"&apikey=1436a691";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
